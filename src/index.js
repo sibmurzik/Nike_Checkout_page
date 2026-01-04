4be993b917accbd8d3572129a99f9f538d176a52
sibmurzik <marrisol@yandex.ru>
2026-01-04 13:27:46 +0100

    4 jan 1st commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './styles.css'
 window.onload = function () {
     let notAvailableSizes = ['EU 33.5', 'EU 34', 'EU 34.5', 'EU 36.5', 'EU 37.5', 'EU 38.5', 'EU 39.5'];
     let selectedSize = null;
-
+    const mobileMenu = document.getElementById('mobile_menu');
+    const favoriteIcon = document.getElementById('favorite_icon');
+    const favoriteInHeader = document.getElementById('favorite_in_header');
     const sizesTable = document.getElementById('sizes_table').children;
 
     for (let size of sizesTable) {
@@ -17,14 +19,24 @@ window.onload = function () {
                 let target = event.target;
                 if(selectedSize == null) {
                     selectedSize = target;
-                    target.classList.toggle("bg-amber-100");
+                    target.classList.remove("bg-white");
+                    target.classList.add("bg-amber-100");
                     return;
                 }
                 if (target === selectedSize) {
-                    target.classList.toggle("bg-white");
+                    if(target.classList.contains("bg-white")) {
+                        target.classList.remove("bg-white");
+                        target.classList.add("bg-amber-100");
+
+                    } else {
+                        target.classList.remove("bg-amber-100");
+                        target.classList.add("bg-white");
+                    }
                 } else {
-                    target.classList.toggle("bg-amber-100");
-                    selectedSize.classList.toggle("bg-white");
+                    target.classList.remove("bg-white");
+                    target.classList.add("bg-amber-100");
+                    selectedSize.classList.remove("bg-amber-100");
+                    selectedSize.classList.add("bg-white");
                     selectedSize = target;
                 }
 
@@ -33,6 +45,37 @@ window.onload = function () {
         }
 
     }
+
+    document.getElementById('open_menu').addEventListener('click', function(event) {
+        mobileMenu.classList.remove("hidden");
+        mobileMenu.classList.add("flex");
+
+    })
+
+    document.getElementById('close_menu').addEventListener('click', function(event) {
+        mobileMenu.classList.remove("flex");
+        mobileMenu.classList.add("hidden");
+    })
+
+    document.getElementById('add_to_favorite').addEventListener('click', function(event) {
+        if (favoriteIcon.classList.contains("fill-none")) {
+            favoriteIcon.classList.remove("fill-none");
+            favoriteIcon.classList.add("fill-black");
+            favoriteInHeader.classList.remove("fill-none");
+            favoriteInHeader.classList.add("fill-black");
+        }
+
+        else {
+            favoriteIcon.classList.remove("fill-black");
+            favoriteIcon.classList.add("fill-none");
+            favoriteInHeader.classList.remove("fill-black");
+            favoriteInHeader.classList.add("fill-none");
+        }
+
+    })
+
+
+
 
 
 
